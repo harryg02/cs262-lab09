ed8541f597ac866f63085cf52ca5ec01d1063d87
harryg02 <94257270+harryg02@users.noreply.github.com>
2023-11-13 22:37:40 -0500

    updated to my own db

--- sql/monopoly-queries.sql
+++ sql/monopoly-queries.sql
@@ -1,34 +1,38 @@
---
--- This SQL script implements sample queries on the Monopoly database.
---
--- @author kvlinden
--- @version Summer, 2015
---
+-- 8.1
+-- Retrieve a list of all the games, ordered by date with the most recent game coming first.
+SELECT * FROM Game
+ORDER BY time DESC;
+-- Retrieve all the games that occurred in the past week.
+SELECT * FROM Game
+WHERE time >= NOW() - INTERVAL 7 DAY;
+-- Retrieve a list of players who have (non-NULL) names.
+SELECT player
+WHERE name IS NOT NULL;
+-- Retrieve a list of IDs for players who have some game score larger than 2000.
+SELECT DISTINCT playerID FROM PlayerGame -- The DISTINCT keyword ensures that each player ID appears only once in the result set, even if a player has multiple game scores greater than 2000.
+WHERE score > 2000;
+-- Retrieve a list of players who have GMail accounts.
+SELECT * FROM Player
+WHERE emailAddress LIKE '%@gmail.com';
+-- 8.2
+-- Retrieve all “The King”’s game scores in decreasing order.
+SELECT PlayerGame.score
+FROM PlayerGame
+JOIN Player ON PlayerGame.playerID = Player.ID
+WHERE Player.name = 'The King'
+ORDER BY PlayerGame.score DESC;
+-- Retrieve the name of the winner of the game played on 2006-06-28 13:20:00.
+SELECT Player.name
+FROM Player, PlayerGame, Game
+WHERE Game.ID = PlayerGame.gameID 
+AND Player.ID = PlayerGame.playerID
+AND Game.time = '2006-06-28 13:20:00'
+ORDER BY PlayerGame.score DESC
+LIMIT 1;
+-- So what does that P1.ID < P2.ID clause do in the last example query (i.e., the from SQL Examples)?
+--The purpose of the query is to find pairs of players who have the same name but different IDs.
 
--- Get the number of Game records.
-SELECT *
-  FROM Game
-  ;
+-- The query that joined the Player table to itself seems rather contrived. Can you think of a realistic situation in which you’d want to join a table to itself?
+-- Yes, For example Finding Pairs of Players with the Same Cash Amount. This would 
+-- require comparing each player's cash amount with every other player's cash amount.
 
--- Get the player records.
-SELECT * 
-  FROM Player
-  ;
-
--- Get all the users with Calvin email addresses.
-SELECT *
-  FROM Player
- WHERE email LIKE '%calvin%'
- ;
-
--- Get the highest score ever recorded.
-  SELECT score
-    FROM PlayerGame
-ORDER BY score DESC
-   LIMIT 1
-   ;
-
--- Get the cross-product of all the tables.
-SELECT *
-  FROM Player, PlayerGame, Game
-  ;
--- sql/monopoly.sql
+++ sql/monopoly.sql
@@ -5,42 +5,64 @@
 -- @version Summer, 2015
 --
 
--- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
+-- Drop previous versions of the tables if they exist, in reverse order of foreign keys.
 DROP TABLE IF EXISTS PlayerGame;
 DROP TABLE IF EXISTS Game;
+DROP TABLE IF EXISTS Property;
 DROP TABLE IF EXISTS Player;
 
--- Create the schema.
-CREATE TABLE Game (
-                      ID SERIAL PRIMARY KEY,
-                      time timestamp
+-- Create the Player table first.
+CREATE TABLE Player (
+    ID integer PRIMARY KEY, 
+    emailAddress varchar(50) NOT NULL,
+    name varchar(50)
 );
 
-CREATE TABLE Player (
-                        ID SERIAL PRIMARY KEY,
-                        email varchar(50) NOT NULL,
-                        name varchar(50)
+-- Now, extend the Player table to include cash.
+ALTER TABLE Player ADD COLUMN cash decimal(10,2) DEFAULT 1500.00; -- Default cash is $1500 in Monopoly.
+
+-- Continue with the creation of other tables.
+CREATE TABLE Game (
+    ID integer PRIMARY KEY,
+    time timestamp
 );
 
 CREATE TABLE PlayerGame (
-                            gameID integer REFERENCES Game(ID),
-                            playerID integer REFERENCES Player(ID),
-                            score integer
+    gameID integer REFERENCES Game(ID), 
+    playerID integer REFERENCES Player(ID),
+    score integer
 );
+
+CREATE TABLE Property (
+    ID integer PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    color varchar(20), -- e.g., 'Red', 'Green', etc.
+    cost decimal(10,2),
+    rent decimal(10,2),
+    house_cost decimal(10,2),
+    hotel_cost decimal(10,2),
+    ownerID integer REFERENCES Player(ID), -- Owner of the property.
+    houses integer DEFAULT 0, -- Number of houses on the property.
+    hotel boolean DEFAULT false -- Whether there's a hotel on the property.
+);
+
+-- Extend the PlayerGame table to track the current location of each player's piece.
+ALTER TABLE PlayerGame ADD COLUMN position integer DEFAULT 0; -- Default position is 'Go' which is position 0.
 
 -- Allow users to select data from the tables.
 GRANT SELECT ON Game TO PUBLIC;
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
+GRANT SELECT ON Property TO PUBLIC;
 
 -- Add sample records.
-INSERT INTO Game(time) VALUES ('2006-06-27 08:00:00');
-INSERT INTO Game(time) VALUES ('2006-06-28 13:20:00');
-INSERT INTO Game(time) VALUES ('2006-06-29 18:41:00');
+INSERT INTO Game VALUES (1, '2006-06-27 08:00:00');
+INSERT INTO Game VALUES (2, '2006-06-28 13:20:00');
+INSERT INTO Game VALUES (3, '2006-06-29 18:41:00');
 
-INSERT INTO Player(email) VALUES ('me@calvin.edu');
-INSERT INTO Player(email, name) VALUES ('king@gmail.com', 'The King');
-INSERT INTO Player(email, name) VALUES ('dog@gmail.com', 'Dogbreath');
+INSERT INTO Player(ID, emailAddress) VALUES (1, 'me@calvin.edu');
+INSERT INTO Player VALUES (2, 'king@gmail.edu', 'The King');
+INSERT INTO Player VALUES (3, 'dog@gmail.edu', 'Dogbreath');
 
 INSERT INTO PlayerGame VALUES (1, 1, 0.00);
 INSERT INTO PlayerGame VALUES (1, 2, 0.00);
@@ -50,3 +72,9 @@ INSERT INTO PlayerGame VALUES (2, 2, 0.00);
 INSERT INTO PlayerGame VALUES (2, 3, 500.00);
 INSERT INTO PlayerGame VALUES (3, 2, 0.00);
 INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
+-- Sample records for the Property table.
+INSERT INTO Property (ID, name, color, cost, rent, house_cost, hotel_cost) VALUES 
+(1, 'Baltic Avenue', 'Purple', 60.00, 2.00, 50.00, 50.00),
+(2, 'Boardwalk', 'Dark Blue', 400.00, 50.00, 200.00, 200.00);
+-- Sample record for a player owning a property.
+UPDATE Property SET ownerID = 1, houses = 2 WHERE ID = 1; -- Player 1 owns Baltic Avenue with 2 houses.
